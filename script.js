a25567daa609aba7e33f46650de9baacc2ccba30
musaev-cood <77251281+musaev-cood@users.noreply.github.com>
2024-05-28 21:11:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ MainButton.setText('ЗАКАЗАТЬ')
 let cart = [];
 let isFirstProductAdded = true;
 
-WebApp.onEvent('mainButtonClicked', function(){
-	//WebApp.sendData(cart);
-	WebApp.showAlert("Вот что в нём хранится " + cartToString() + " и всё)" );
-});
-
-
 function cartToString() {
         return cart.map(item =>
             `Название: ${item.name}, Цена: ${item.price} ₽, Количество: ${item.quantity}`
         ).join('\n');
 }
-	
+
+WebApp.onEvent('mainButtonClicked', function(){
+	//WebApp.sendData(cart);
+	WebApp.showAlert("Вот что в нём хранится " + cartToString() + " и всё)" );
+});
+
 function cartDispatcher() {
     const footer = document.querySelector('footer');
     const scrollToCategoriesButton = document.getElementById('scrollToCategoriesButton');
