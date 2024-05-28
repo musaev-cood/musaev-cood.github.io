2c5c795ab40c879ce77daaf1badf9e7f65a0db5c
musaev-cood <77251281+musaev-cood@users.noreply.github.com>
2024-05-28 21:32:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let isFirstProductAdded = true;
 
 WebApp.onEvent('mainButtonClicked', function(){
     WebApp.showAlert("Вот что в нём хранится");
-    // WebApp.sendData(cartToString());
+    WebApp.sendData(cartToString());
     // WebApp.showAlert("Вот что в нём хранится " + cartToString() + " и всё)" );
 });
 
