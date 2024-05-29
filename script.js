a1e0576d1ee80ce8ad331c8876a4812a300cee44
musaev-cood <77251281+musaev-cood@users.noreply.github.com>
2024-05-29 17:13:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function OpenCartButton(element) {
 }
 
 WebApp.onEvent('mainButtonClicked', function(){
-    WebApp.showAlert("123");
+    WebApp.showAlert("1243");
     let NameButton = MainButton.gettext();
     WebApp.showAlert("13323");
     if(NameButton === "КОРЗИНА"){
