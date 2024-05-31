4b47d0d107b0fe87666e7cb5b729cd4549d92b7c
musaev-cood <77251281+musaev-cood@users.noreply.github.com>
2024-05-31 14:47:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,384 @@
-import asyncio
-import logging
-import sys
-from os import getenv
+let WebApp = window.Telegram.WebApp;
+WebApp.expand();
+MainButton = WebApp.MainButton;
+BackButton = WebApp.BackButton;
+MainButton.setText('КОРЗИНА');
+let nav = 'КОРЗИНА'
+let cart = [];
+let isFirstProductAdded = true;
+const deliveryCost = 150;
+function disableScroll() {
+    document.body.style.overflow = 'hidden';
+}
 
-from aiogram import Bot, Dispatcher, F
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
-from aiogram.types.web_app_info import WebAppInfo
-from aiogram.types.web_app_data import WebAppData
+function enableScroll() {
+    document.body.style.overflow = 'auto';
+}
 
-TOKEN = getenv("BOT_TOKEN")
+function GetCartString(){
+    const totalCost = cart.reduce((sum, item) => sum + (item.price * item.quantity), 0);
+    const totalWithDelivery = totalCost + deliveryCost;
+    const comment = document.getElementById('orderComment').value;
+    const customerNumber = document.getElementById('phoneNumber').value.toString();
+    const deliveryAddress = document.getElementById('adresuser').value.toString();
 
-bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher()
+    const cartString = cart.map((item, index) =>
+      `${index + 1}) "${item.name}"\nЦена: ${item.price} ₽\nКол-во: ${item.quantity}`
+    ).join('\n\n') + `\n\nСумма с доставкой ${totalWithDelivery} ₽`;
 
+    const orderString = `${cartString}\n\nКомментарий к заказу:\n${comment}\n\nНомер покупателя:\n${customerNumber}\n\nАдрес доставки:\n${deliveryAddress}`;
+    return orderString.toString()
+}
+function OpenCartButton(){
+    disableScroll()
+    updateCartDisplay();
+    nav = 'ЗАКАЗАТЬ'
+    const cartElement = document.getElementById('cart');
+    cartElement.classList.remove('hidden');
+    cartElement.classList.add('visible');
+    MainButton.setText('ЗАКАЗАТЬ');
+    document.getElementById('phoneNumber').addEventListener('input', function() {
+        UpdateOrderButton();
+    });
+    document.getElementById('phoneNumber').addEventListener('keypress', function(event) {
+        const key = event.key;
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    webApp = WebAppInfo(url="https://musaev-cood.github.io/")
+        if (isNaN(parseInt(key))) {
+            event.preventDefault();
+        }
+    });
 
-    keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="GoyMarket", web_app=webApp)]], resize_keyboard=True)
+    document.getElementById('adresuser').addEventListener('input', function() {
+        UpdateOrderButton();
+    });
 
-    await message.reply(text="Нажмите на кнопку что бы открыть GoyMarket", reply_markup=keyboard)
+    function UpdateOrderButton(){
+        const userNumber = document.getElementById('phoneNumber').value.toString();
+        const userAdres = document.getElementById('adresuser').value.toString();
 
+        if(userAdres.length > 4 && userNumber.length === 12){
+            console.log('1')
+        }else{
+            console.log('2')
+        }
+    }
+}
+function EditCart() {
+    enableScroll()
+    nav = 'КОРЗИНА'
+    const cartElement = document.getElementById('cart');
+    cartElement.classList.remove('visible');
+    cartElement.classList.add('hidden');
 
-@dp.message(F.web_app_data)
-async def webapp_data_handler(message: WebAppData) -> None:
-    user_id = message.from_user.id
-    username = message.from_user.username
-    text = f"!НОВЫЙ ЗАКАЗ!\nот @{username}\nuserID - {user_id}\n{message.text}"
-    await bot.send_message(chat_id=800227103, text=text)
-    await message.reply(text=message.web_app_data.data)
+    BackButton.hide();
+    MainButton.show();
+    MainButton.setText('КОРЗИНА');
+}
 
+WebApp.onEvent('backButtonClicked', function() {
+    EditCart();
+});
 
-async def main() -> None:
-    try:
-        await dp.start_polling(bot)
-    except asyncio.CancelledError:
-        print("Polling was cancelled.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        print("Closing the event loop.")
-        await bot.session.close()
+document.addEventListener('click', function(event) {
+        const target = event.target;
+        const textInputs = document.querySelectorAll('input[type="text"], input[type="tel"], textarea');
+        textInputs.forEach(function(input) {
+            if (!input.contains(target)) {
+                input.blur();
+            }
+        });
+    });
 
+WebApp.onEvent('mainButtonClicked', function(){
+    updateCartDisplay();
+    if(nav === "КОРЗИНА"){
+        disableScroll()
+        const cartElement = document.getElementById('cart');
+        cartElement.classList.remove('hidden');
+        cartElement.classList.add('visible');
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+        nav = 'ЗАКАЗАТЬ'
+        MainButton.setText('ЗАКАЗАТЬ');
+        BackButton.show();
+        MainButton.hide();
+        document.getElementById('phoneNumber').addEventListener('input', function(event) {
+            disableScroll()
+            const phoneNumber = event.target.value.replace(/\D/g, '');
+
+            if (phoneNumber === '') {
+                event.target.value = '+7';
+            } else {
+                if (!phoneNumber.startsWith('7')) {
+                    event.target.value = '+7' + phoneNumber.substring(1);
+                }
+            }
+            UpdateOrderButton();
+        });
+        document.getElementById('phoneNumber').addEventListener('keypress', function(event) {
+            const key = event.key;
+
+            if (isNaN(parseInt(key))) {
+                event.preventDefault();
+            }
+        });
+
+        document.getElementById('adresuser').addEventListener('input', function() {
+            disableScroll()
+            UpdateOrderButton();
+        });
+
+        function UpdateOrderButton(){
+            const userNumber = document.getElementById('phoneNumber').value.toString();
+            const userAdres = document.getElementById('adresuser').value.toString();
+
+            if(userAdres.length > 4 && userNumber.length === 12){
+                MainButton.show();
+            }else{
+                MainButton.hide();
+            }
+        }
+    }else{
+        nav = 'КОРЗИНА'
+        WebApp.sendData(GetCartString());
+        MainButton.setText('КОРЗИНА');
+    }
+});
+
+function updateCartDisplay() {
+    const orderContainer = document.getElementById('cart-container');
+    orderContainer.innerHTML = '';
+
+    const allSumPriceElement = document.querySelector('.AllSumPrice');
+    allSumPriceElement.innerText = `Общая сумма с учётом доставки: ${deliveryCost} ₽`;
+
+    cart.forEach(item => {
+        const orderItem = document.createElement('div');
+        orderItem.classList.add('order-item');
+
+        const img = document.createElement('img');
+        img.src = item.ProductURL;
+        orderItem.appendChild(img);
+
+        const itemInfo = document.createElement('div');
+        itemInfo.classList.add('item-info');
+        const itemName = document.createElement('p');
+        itemName.textContent = `"${item.name}", ${item.quantity} шт.`;
+        itemInfo.appendChild(itemName);
+        orderItem.appendChild(itemInfo);
+
+        const itemPrice = document.createElement('div');
+        itemPrice.classList.add('item-price');
+        itemPrice.id = 'price-item';
+        itemPrice.innerHTML = `${item.price} <span>₽</span>`;
+        orderItem.appendChild(itemPrice);
+
+        orderContainer.appendChild(orderItem);
+
+        const allSumPriceElement = document.querySelector('.AllSumPrice');
+
+        const number = parseFloat(allSumPriceElement.textContent.match(/\d+/)[0]);
+
+        allSumPriceElement.innerText = `Общая сумма с учётом доставки: ${number+(item.price*item.quantity)} ₽`;
+    });
+}
+
+function cartDispatcher() {
+    const footer = document.querySelector('footer');
+    const scrollToCategoriesButton = document.getElementById('scrollToCategoriesButton');
+    const totalSum = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+    if (cart.length > 0 && totalSum > 100){
+        footer.style.display = 'none';
+        scrollToCategoriesButton.style.bottom = '7px';
+        WebApp.MainButton.show()
+    }else{
+        footer.style.display = 'inline-block';
+        scrollToCategoriesButton.style.bottom = '50px';
+        WebApp.MainButton.hide()
+    }
+}
+
+function AddNewProduct(productURL ,productName, productPrice){
+    if (('ontouchstart' in window || navigator.maxTouchPoints) && isFirstProductAdded && productPrice > 40000) {
+            WebApp.showAlert('Привет, друг! У нас можно заказать от 400 ₽.\n' +
+            'Кнопка \'Заказать\' появится, как только ты наберешь продукты в корзину.')
+            isFirstProductAdded = false;
+        }
+    cart.push({
+            ProductURL: productURL,
+            name: productName,
+            price: productPrice,
+            quantity: 1
+        });
+}
+
+function DeleteThisProduct(productName){
+    cart = cart.filter(item => item.name !== productName);
+}
+
+function UpdateQuantity(index, quantityElement){
+    cart[index].quantity = parseInt(quantityElement.textContent)
+}
+
+function AddToCartButton(element) {
+    if ('ontouchstart' in window || navigator.maxTouchPoints){
+        WebApp.HapticFeedback.impactOccurred("rigid")
+    }
+
+    const deleteButton = element.nextElementSibling;
+    const quantityElement = element.nextElementSibling.nextElementSibling;
+    const productElement = element.closest('.product');
+
+    const productURL = productElement.querySelector('.product-URL').src;
+    const productName = element.parentElement.querySelector('.product-title').textContent;
+    const productPrice = parseFloat(element.parentElement.querySelector('.product-price').textContent);
+
+    deleteButton.style.display = 'inline-block';
+    quantityElement.style.display = 'inline-block';
+
+    let currentQuantity = parseInt(quantityElement.textContent, 10) || 0;
+
+    if (currentQuantity === 0) {
+        AddNewProduct(productURL, productName, productPrice);
+
+        quantityElement.classList.add('fade-in');
+        setTimeout(() => {
+            quantityElement.classList.remove('fade-in');
+        }, 200);
+
+        deleteButton.style.transition = 'padding 0.2s';
+        element.textContent = "+";
+        element.style.fontWeight = 800;
+        element.style.padding = '.3em 3em calc(.3em + 3px)';
+
+        setTimeout(() => {
+            deleteButton.style.padding = '.3em 1.25em calc(.3em + 3px)';
+            deleteButton.style.opacity = 1;
+            element.style.padding = '.3em 1em calc(.3em + 3px)';
+        }, 100);
+    } else {
+        quantityElement.classList.add('bounce-in');
+        setTimeout(() => {
+            quantityElement.classList.remove('bounce-in');
+        }, 100);
+    }
+    const index = cart.findIndex(item => item.name === productName);
+    if (index !== -1 && currentQuantity < 9) {
+        quantityElement.textContent = currentQuantity + 1;
+        UpdateQuantity(index, quantityElement)
+    }
+
+    cartDispatcher();
+}
+
+function deleteFromCartButton(element) {
+    if ('ontouchstart' in window || navigator.maxTouchPoints){
+        WebApp.HapticFeedback.impactOccurred("rigid")
+    }
+
+    const quantityElement = element.nextElementSibling;
+    const addButton = element.previousElementSibling;
+
+    const productName = element.parentElement.querySelector('.product-title').textContent;
+
+    let currentQuantity = parseInt(quantityElement.textContent, 10) || 0;
+    quantityElement.textContent = Math.max(0, currentQuantity - 1).toString();
+
+    if (currentQuantity <= 1) {
+        DeleteThisProduct(productName);
+
+        quantityElement.classList.add('fade-out');
+        setTimeout(() => {
+            quantityElement.classList.remove('fade-out');
+            quantityElement.style.display = 'none';
+        }, 100);
+
+        addButton.style.transition = 'padding 0.15s';
+
+        element.style.transition = 'padding 0.15s';
+        element.style.transition = 'opacity 0.2s';
+
+        element.style.display = 'none';
+        element.style.opacity = 0;
+        element.style.padding = '.3em 0.1em calc(.3em + 3px)';
+
+        addButton.style.fontWeight = 800;
+        addButton.style.padding = '.3em 1.5em calc(.3em + 3px)';
+        setTimeout(() => {
+            addButton.style.padding = '.3em 0.7em calc(.3em + 3px)';
+        }, 50);
+        setTimeout(() => {
+            addButton.textContent = "ДОБАВИТЬ";
+        }, 100);
+
+    } else {
+        const index = cart.findIndex(item => item.name === productName);
+        if (index !== -1) {
+            UpdateQuantity(index, quantityElement);
+            quantityElement.classList.add('bounce-out');
+            setTimeout(() => {
+                quantityElement.classList.remove('bounce-out');
+            }, 100);
+        }
+    }
+    cartDispatcher();
+}
+
+function rotateAnimation(element) {
+    let clickedElement = event.target;
+
+    if (clickedElement.tagName.toLowerCase() === 'img') {
+        if ('ontouchstart' in window || navigator.maxTouchPoints){
+            WebApp.HapticFeedback.impactOccurred("rigid")
+        }
+        let img = element.querySelector('img');
+        if (!img.classList.contains('rotate')) {
+            img.classList.add('rotate');
+            setTimeout(function () {
+                img.classList.remove('rotate');
+            }, 500);
+        }
+    }
+}
+
+window.onscroll = function () {
+    const scrollToCategoriesButton = document.getElementById('scrollToCategoriesButton');
+    const shouldShowButton = document.body.scrollTop > 600 || document.documentElement.scrollTop > 600;
+
+    if (shouldShowButton !== scrollToCategoriesButton.classList.contains('visible')) {
+        scrollToCategoriesButton.classList.toggle('visible');
+    }
+}
+
+function scrollToTarget(targetId) {
+    const targetElement = document.getElementById(targetId);
+    WebApp.HapticFeedback.impactOccurred("rigid")
+    if (targetElement) {
+        const targetPosition = targetElement.offsetTop;
+        const currentPosition = window.scrollY;
+        const distance = targetPosition - currentPosition;
+
+        const duration = 1000;
+        let start = null;
+
+        function step(timestamp) {
+            if (!start) start = timestamp;
+            const progress = timestamp - start;
+
+            window.scrollTo(0, easeInOutQuad(progress, currentPosition, distance, duration));
+
+            if (progress < duration) {
+                requestAnimationFrame(step);
+            }
+        }
+
+        function easeInOutQuad(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        }
+
+        requestAnimationFrame(step);
+    }
+}
